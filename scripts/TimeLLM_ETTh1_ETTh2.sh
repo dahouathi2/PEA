158a18c0a7178e3e862487e658febdbe8af0bb92
dahouathi2 <wadhah.dahouathi@loreal.com>
2024-05-21 08:49:55 +0000

    Created zero_shot with timellm

--- scripts/TimeLLM_ETTh1_ETTh2.sh
+++ scripts/TimeLLM_ETTh1_ETTh2.sh
@@ -4,16 +4,16 @@ llama_layers=32
 
 master_port=00097
 num_process=8
-batch_size=24
+batch_size=100
 d_model=32
 d_ff=128
 
 comment='TimeLLM-ETTh1_ETTh2'
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_pretrain.py \
+accelerate launch --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_pretrain.py \
   --task_name long_term_forecast \
   --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
+  --root_path ./dataset \
   --data_path_pretrain ETTh1.csv \
   --data_path ETTh2.csv \
   --model_id ETTh1_ETTh2_512_96 \
@@ -21,13 +21,13 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --data_pretrain ETTh1 \
   --data ETTh2 \
   --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 96 \
+  --seq_len 26 \
+  --label_len 4 \
+  --pred_len 4 \
   --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
+ --enc_in 9 \
+  --dec_in 9 \
+  --c_out 9 \
   --des 'Exp' \
   --itr 1 \
   --d_model $d_model \
@@ -37,88 +37,5 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --llm_layers $llama_layers \
   --train_epochs 5 \
   --model_comment $comment
-
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_pretrain.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path_pretrain ETTh1.csv \
-  --data_path ETTh2.csv \
-  --model_id ETTh1_ETTh2_512_192 \
-  --model $model_name \
-  --data_pretrain ETTh1 \
-  --data ETTh2 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 192 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model 32 \
-  --d_ff 128 \
-  --batch_size $batch_size \
-  --learning_rate 0.02 \
-  --llm_layers $llama_layers \
-  --train_epochs 5 \
-  --model_comment $comment
-
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_pretrain.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path_pretrain ETTh1.csv \
-  --data_path ETTh2.csv \
-  --model_id ETTh1_ETTh2_512_336 \
-  --model $model_name \
-  --data_pretrain ETTh1 \
-  --data ETTh2 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 336 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model $d_model \
-  --d_ff $d_ff \
-  --batch_size $batch_size \
-  --lradj 'COS'\
-  --learning_rate 0.001 \
-  --llm_layers $llama_layers \
-  --train_epochs 5 \
-  --model_comment $comment
-
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_pretrain.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path_pretrain ETTh1.csv \
-  --data_path ETTh2.csv \
-  --model_id ETTh1_ETTh2_512_720 \
-  --model $model_name \
-  --data_pretrain ETTh1 \
-  --data ETTh2 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 720 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model $d_model \
-  --d_ff $d_ff \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs 5 \
-  --model_comment $comment
+  --llm_model GPT2 \
+  --llm_dim 768
--- scripts/TimeLLM_pretrain.sh
+++ scripts/TimeLLM_pretrain.sh
@@ -5,25 +5,25 @@ llama_layers=32
 
 master_port=00098
 num_process=1
-batch_size=6
+batch_size=24
 d_model=32
 d_ff=128
 
-comment='TimeLLM-EAN'
+comment='TimeLLM-Pretrain-26-8640'
 
 accelerate launch --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset \
-  --data_path data.csv \
-  --model_id EAN_512_96 \
+  --data_path ETTh1.csv \
+  --model_id Pretrain \
   --model $model_name \
-  --data ean \
-  --features M \
-  --target sold_units \
-  --seq_len 13 \
-  --label_len 1 \
-  --pred_len 1 \
+  --data ETTh1 \
+  --features MS \
+  --target OT \
+  --seq_len 26 \
+  --label_len 4 \
+  --pred_len 4 \
   --factor 3 \
   --enc_in 9 \
   --dec_in 9 \
@@ -39,4 +39,3 @@ accelerate launch --mixed_precision bf16 --num_processes $num_process --main_pro
   --model_comment $comment \
   --llm_model GPT2 \
   --llm_dim 768 
-
